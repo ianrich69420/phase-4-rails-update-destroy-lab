8ab1d8a61b8a5ffca8b9e31b14617e80ef8bbc5e
Ian Richard <ian.orieko@student.moringaschool.com>
2023-03-25 02:55:48 +0000

    Completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /plants
   def index
@@ -16,6 +17,18 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  def update
+    plant = Plant.find(params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    render json: {}, status: :no_content
   end
 
   private
